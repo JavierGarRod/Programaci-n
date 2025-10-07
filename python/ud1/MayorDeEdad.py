f1295a3c234948be57103c646c89f786a674bf30
JavierGarRod <fgarrod408@g.educaand.es>
2025-10-07 09:53:14 +0200

    subo actividades 7/10

---
+++ python/ud1/MayorDeEdad.py
@@ -0,0 +1,8 @@
+edad=int(input("Dime tu edad: "))
+while edad!=0:
+    if edad ==18:
+        print("Tienes derecho al bono cultural")
+    else:
+        print("Eres menor de edad, no tienes derecho al bono cultural")
+    edad=int(input("Dime tu edad: "))
+print("Fin")
