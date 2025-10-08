a4c13cd977e6ee4e1a545734855d6b1bc8d4101c
JavierGarRod <fgarrod408@g.educaand.es>
2025-10-08 10:02:21 +0200

    Subo dia 8

---
+++ python/ud1/Contraseña.py
@@ -0,0 +1,6 @@
+contraseña=1234
+contra=int(input("Introduzca la contraseña: "))
+while contra != contraseña:
+    contra=int(input("Contraseña incorrecta. Intentalo de nuevo: "))
+print("Contraseña correcta")
+print("Bienvenido")
---
+++ python/ud1/SumaDeNumeros.py
@@ -0,0 +1,6 @@
+suma=0
+numero1=int(input("Dime un número: "))
+while numero1!=0:
+    suma=suma + numero1
+    numero1=int(input("Dime otro número: "))
+    print(suma)
