6ba56e238343c287d94cdd5fd53f1f61b60c9750
JavierGarRod <fgarrod408@g.educaand.es>
2026-01-30 12:38:34 +0100

    Subo actividades

---
+++ Java/Programacion/src/programacion/Tema1/ActividadesU1Java/OrientacionObjeto/Ej1Libro/Libro.java
@@ -0,0 +1,4 @@
+package programacion.Tema1.ActividadesU1Java.OrientacionObjeto.Ej1Libro;
+
+public class Libro {
+}
---
+++ Java/Programacion/src/programacion/Tema1/ActividadesU1Java/OrientacionObjeto/Ej2Gatos/Gato.java
@@ -0,0 +1,4 @@
+package programacion.Tema1.ActividadesU1Java.OrientacionObjeto.Ej2Gatos;
+
+public class Gato {
+}
