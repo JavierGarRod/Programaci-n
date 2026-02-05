4fdefe4b358cb6d5a43b09f3b86712d24b34220c
JavierGarRod <fgarrod408@g.educaand.es>
2026-02-05 09:56:21 +0100

    Subo actividades 5

---
+++ Java/Programacion/src/programacion/Tema1/ActividadesU1Java/OrientacionObjeto/DronesReparto/EstadoDron.java
@@ -0,0 +1,5 @@
+package programacion.Tema1.ActividadesU1Java.OrientacionObjeto.Drones;
+
+public enum EstadoDron {
+    EN_REPOSO, EN_RUTA, CARGANDO, FUERA_DE_SERVICIO
+}
---
+++ Java/Programacion/src/programacion/Tema1/ActividadesU1Java/OrientacionObjeto/RobotLimpieza/EstadoHabitacion.java
@@ -0,0 +1,4 @@
+package programacion.Tema1.ActividadesU1Java.OrientacionObjeto.RobotLimpieza;
+
+public enum EstadoHabitacion {
+}
